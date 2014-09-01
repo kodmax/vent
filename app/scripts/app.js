4b671bf8c9c8a45a7143a7682c8ce6a23aad41e5
Marcin <mm.kodmax@gmail.com>
2014-09-01 21:47:11 +0200

    fwef

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,35 +3,18 @@ require.config({
 	baseUrl: 'scripts/',
 	
 	paths: {
-		'angular.src': '.3rd-party/angularjs/angular',
-		'ng': 'amd-wrappers/angular',
 		'modernizr': 'amd-wrappers/modernizr',
 		'modernizr.src': '.3rd-party/modernizr/modernizr',
 		'jquery': 'amd-wrappers/jquery'
 	},
 	
 	shim: {
-		'angular.src': { exports: 'angular'	},
 		'modernizr.src': { exports: 'Modernizr'	}
 	}
 });
 
-require(['ng'], function (ng) {
+require([], function () {
 	'use strict';
-	
-	var myApp = ng.module('myApp', ['b'], function () {
-		console.log('a config');
-	});
-	
-	ng.module('b', [], function () {
-		console.log('b config');
-	});
-	
-	myApp.run(function () {
-		console.log('app run');
-	});
-	
-	myApp.controller('myCtrl', function($scope) {
-	    console.log("app controller");
-	});
+
+	console.log('app start');
 });
