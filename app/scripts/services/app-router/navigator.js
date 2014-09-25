6b8501df7416b71d985c6637f4ddbae46ea643a0
mm.kodmax@gmail.com <mm.kodmax@gmail.com>
2014-09-25 20:36:28 +0200

    app router navigator completed

--- app/scripts/services/app-router/navigator.js
+++ app/scripts/services/app-router/navigator.js
@@ -9,32 +9,52 @@ define([], function() {
 	var Navigator = function (promise) {
 		var contexts = {};
 		
+		var currentStateTs;
+		var previousStateTs;
 		var previousContext;
 		var context;
 		addEventListener('popstate', function (event) {
-			console.log('popstate', event);
 			var state = event.state;
+			
 			if (state && state ['state-id'] && contexts [state ['state-id']]) {
 				context = contexts [state ['state-id']];
+				currentStateTs = state ['state-id'];
 			}
 		});
 		
-		addEventListener('hashchange', function (event) {
-			console.log('hashchange', event, context);
+		var hashchange = function (hash) {
 			if (!context) {
 				var stateId = new Date().getTime();
+				currentStateTs = stateId;
 				
-				context = contexts [stateId] = promise.create(event.newURL);
-				history.replaceState({ 'state-id': stateId });				
+				if (previousStateTs) {
+					// there is a known issue that some backward history entries will get disposed if users starts app and navigate backward to old instances urls.
+					Object.keys(contexts).forEach(function (ts) {
+						if (ts > previousStateTs) {
+							promise.dispose(contexts [ts]);
+							delete contexts [ts];
+						}
+					});
+				}
+				
+				context = contexts [stateId] = promise.create(hash);
+				history.replaceState({ 'state-id': stateId });
 			}
 			
 			previousContext && promise.navout(previousContext);
 			promise.navin(context);
 			
+			previousStateTs = currentStateTs;
 			previousContext = context;
+			currentStateTs = null;
 			context = null;
+		};
+		
+		addEventListener('hashchange', function (event) {
+			hashchange(location.hash); 
 		});
 		
+		hashchange(location.hash);
 	};
 	
 	return Navigator;
