ac81e3e81b600cc342780c719136900309a0f4e6
mm.kodmax@gmail.com <mm.kodmax@gmail.com>
2014-09-28 10:53:14 +0200

    some styling

--- app/scripts/rivets-bootsrap/formatters.js
+++ app/scripts/rivets-bootsrap/formatters.js
@@ -1,4 +1,4 @@
-define([ 'rivets', 'exception', 'card-url'], function(rivets, Exception, cardUrl) {
+define([ 'rivets', 'exception', 'card-url', 'translate'], function(rivets, Exception, cardUrl, t) {
 	'use strict';
 
 	var formatters = function() {
@@ -8,19 +8,20 @@ define([ 'rivets', 'exception', 'card-url'], function(rivets, Exception, cardUrl
 
 		rivets.formatters.currency = {
 		    read : function(value) {
-			    return value.toFixed(2) + " $";
-		    },
-		    publish : function(value) {
-			    return Math.round(parseFloat(value) * 100) / 100;
+			    return '$' + value.toFixed(2);
 		    }
+		};
+		
+		rivets.formatters.translate = {
+			read: function (text) {
+				console.log(text, arguments, this);
+				return t(text);
+			}
 		};
 
 		rivets.formatters ['gallery-img-url'] = {
 		    read: function(value) {
 			    return 'images/products/' + value;
-		    },
-		    publish: function(value) {
-			    throw new Exception ('oops, unable to convert back');
 		    }
 		};
 	};
